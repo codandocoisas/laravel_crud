a3b278fe0ce6c4ac9d82d53fb6330ba166788c28
Bruno <bruno_moeller@proton.me>
2023-01-12 21:16:31 -0300

    added new required fields user migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,8 +18,12 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'full_name',
         'email',
+        'cpf',
+        'state',
+        'city',
+        'zip_code',
         'password',
     ];
 
