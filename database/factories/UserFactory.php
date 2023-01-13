16a92bd7ff50368590862ba88e88613ef7710ca8
Bruno <bruno_moeller@proton.me>
2023-01-13 15:02:06 -0300

    added seeding for database testing

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,25 +4,37 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Faker\Provider\en_AU\Address;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
-class UserFactory extends Factory
+function generateRandomString($length = 10) {
+    return substr(str_shuffle(str_repeat($x='0123456789', ceil($length/strlen($x)) )),1,$length);
+}
+
+ class UserFactory extends Factory
 {
     /**
      * Define the model's default state.
      *
      * @return array<string, mixed>
      */
+
+
     public function definition()
     {
         return [
             'name' => fake()->name(),
+            'full_name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'cpf' => generateRandomString($length = 11),
+            'city' => fake()->city(),
+            'state' => Address::state(),
+            'zip_code' => generateRandomString($length = 8),
         ];
     }
 
