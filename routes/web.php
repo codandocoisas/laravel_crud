651ff598acf5430595cb1aa0fb32811901f98a08
Bruno <bruno_moeller@proton.me>
2023-01-13 15:37:03 -0300

    added authorization controller and login routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,3 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::get('login', [AuthController::class, 'create']) -> name('login');
+Route::post('login', [AuthController::class, 'store']) -> name('login.store');
+Route::delete('logout', [AuthController::class, 'destroy']) -> name('logout');
